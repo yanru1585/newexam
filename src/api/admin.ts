3af877214dea56aea062eb047cd0d9cd3a6e929e
张雨杰 <zhangrong092400>
2023-02-21 20:16:29 +0800

    阅卷管理

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -8,3 +8,17 @@ interface Iadmin{
 export const Loginteach:Iadmin=(username:string,pass:string):Promise<AxiosResponse<any>>=>{
   return post('/teacher/checklogin',{username,pass})
 }
+// 阅卷管理列表
+interface Iist{
+  (page:number,psize:number,isread:number,key:string):Promise<AxiosResponse<any>>
+}
+export const Ilist:Iist=(page:number,psize:number,isread:number,key:string):Promise<AxiosResponse<any>>=>{
+  return get('/test/list',{page,psize,isread,key})
+}
+// 阅卷查看
+interface Isee{
+  (page:number,psize:number,testid:number,state:string,key:string):Promise<AxiosResponse<any>>
+}
+export const ISee:Isee=(page:number,psize:number,testid:number,state:string,key:string):Promise<AxiosResponse<any>>=>{
+  return get('/student/test',{page,psize,testid,key,state})
+}
