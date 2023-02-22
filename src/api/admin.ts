e888427de49b00459a5ae518a18e568e85aa4670
cxy <za2112804687>
2023-02-22 20:33:58 +0800

    角色

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -84,11 +84,11 @@ export const rolelist=(params:any):Promise<AxiosResponse<any>>=>{
 
 export const roledel=(id:any):Promise<AxiosResponse<any>>=>{
   return get('/role/delete',{id})
-<<<<<<< HEAD
+
 }
 
-=======
-}//角色删除
+
+//角色删除
 
 export const menulsit=(params:any):Promise<AxiosResponse<any>>=>{
   return get('/menu/list',params)
@@ -98,4 +98,3 @@ export const menulsit=(params:any):Promise<AxiosResponse<any>>=>{
 export const roleadd=(params:any):Promise<AxiosResponse<any>>=>{
   return get('/role/add',params)
 }//角色添加
->>>>>>> mian
