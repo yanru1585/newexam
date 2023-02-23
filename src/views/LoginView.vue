db76085c5fab0100cb80a106a06ec3233d499778
yanru1585 <yanru1585@163.com>
2023-02-23 18:20:40 +0800

    修改账号密码验证

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -78,9 +78,9 @@ const ruleForm = reactive({
 const {loginData}=toRefs(ruleForm)
 const rules = reactive<FormRules>({
 
-  username: [{ required: true, message: '请输入账号', trigger: 'blur' },{ min: 3, max: 5, message: '账号为3到5个字符', trigger: 'blur' },],
+  username: [{ required: true, message: '请输入账号', trigger: 'blur' },{ min: 3, max: 10, message: '账号为3到10个字符', trigger: 'blur' },],
   
-  pass: [{ required: true, message: '请输入密码', trigger: 'blur' },{ min: 3, max: 5, message: '密码为3到5个字符', trigger: 'blur' },],
+  pass: [{ required: true, message: '请输入密码', trigger: 'blur' },{ min: 3, max: 10, message: '密码为3到10个字符', trigger: 'blur' },],
 });
 
 
