7c24f2c4c62344b1b2d13be394d80ea759497ff6
cxy <za2112804687>
2023-02-25 07:46:44 +0800

    学员

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'vant/lib/index.css'
-import Vant from './utils/vantui'
 import './assets/css/iconfont.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
