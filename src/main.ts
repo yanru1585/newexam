5128772eb091972149f7dff6ef021bead740c1f7
yanru1585 <yanru1585@163.com>
2023-02-24 16:59:29 +0800

    修改main

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'vant/lib/index.css'
-import Vant from './utils/vantui'
 import './assets/css/iconfont.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
@@ -19,11 +18,5 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-<<<<<<< HEAD
 
-
-
-
-=======
->>>>>>> 71a5a410544d1a7ae71fe78da0f2e50ee932db16
 
