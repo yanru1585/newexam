a4cb26df7affe5831974ee0c60a33e604948e964
张雨杰 <zhangrong092400>
2023-02-28 16:05:53 +0800

    完善了班级修改回显

--- src/views/LeftMenu/classes/Classdialog.vue
+++ src/views/LeftMenu/classes/Classdialog.vue
@@ -5,7 +5,7 @@
   >
     <el-form
       ref="ruleFormRef"
-      :model="ruleForm"
+      :model="Idataa"
       :rules="rules"
       label-width="120px"
       class="demo-ruleForm"
@@ -13,10 +13,10 @@
       status-icon
     >
       <el-form-item label="班级名称" prop="name">
-        <el-input v-model="ruleForm.name" />
+        <el-input v-model="Idataa.name" />
       </el-form-item>
       <el-form-item label="部门" prop="name">
-        <el-cascader :options="dataa.arr" :props="props" clearable @change="handleChange"/>
+        <el-cascader v-model="Idataa.depid" :options="dataa.arr" :props="props" clearable @change="handleChange"/>
       </el-form-item>
     </el-form>
 
@@ -52,11 +52,7 @@ const a = defineProps({
 console.log(a.carr);
 const formSize = ref('default');
 const ruleFormRef = ref<FormInstance>();
-const ruleForm = reactive({
-  name: a.carr.name,
-  depid: 0,
-  id: a.carr.id,
-});
+
 
 const rules = reactive<FormRules>({
   name: [{ required: true, message: '请输入班级名称', trigger: 'blur' }],
@@ -65,18 +61,27 @@ const rules = reactive<FormRules>({
 // 级联选择器change事件
 const handleChange = (value:any) => {
   console.log(value)
-  ruleForm.depid=value
+  Idataa.depid=value
 }
-
 // 确定按钮
+interface Idata{
+  name: string,
+  depid: number,
+  id: number,
+}
+const Idataa:Idata=reactive({
+  name: a.carr.name,
+  depid: a.carr.ids,
+  id: a.carr.id,
+})
 const add = async (formEl: FormInstance | undefined) => {
   if (!formEl) return;
   await formEl.validate(async (valid, fields) => {
     if (valid) {
       const res: any = await classesadd(
-        ruleForm.name,
-        ruleForm.depid,
-        ruleForm.id
+        Idataa.name,
+        Idataa.depid,
+        Idataa.id
       );
       // console.log(res);
       if(res.errCode === 10000){
