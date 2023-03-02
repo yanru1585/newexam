38e29c193e9577df8d7431d9cc392815c31d7649
yanru1585 <yanru1585@163.com>
2023-03-02 20:26:36 +0800

    考试删除

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -15,7 +15,9 @@ export const list=(params:any):Promise<AxiosResponse<any>>=>{
 export const dele=(ids:any):Promise<AxiosResponse<any>>=>{
   return post('/test/deleteall',{ids})
 }//考试批量删除
-
+export const deles=(id:any):Promise<AxiosResponse<any>>=>{
+   return get('/test/delete',{id})
+  }//考试删除
 export const AddText=(params:any):Promise<AxiosResponse<any>>=>{
   return post('/test/add',params)
 }//考试添加
@@ -83,7 +85,7 @@ export const classesdeles=(ids:any):Promise<AxiosResponse<any>>=>{
 }//班级批量删除
 
 export const classesad=(params:any):Promise<AxiosResponse<any>>=>{
-  return post('/student/add',params)
+ return post('/student/add',params)
 }//添加学生
 export const studentlist=(params:any):Promise<AxiosResponse<any>>=>{
   return get('/student/list',params)
