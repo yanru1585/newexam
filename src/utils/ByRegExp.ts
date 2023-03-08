e0043ec9089531355189bd5f7b5a1fbe47a28104
yanru1585 <yanru1585@163.com>
2023-03-08 19:56:38 +0800

    页面修整

---
+++ src/utils/ByRegExp.ts
@@ -0,0 +1,13 @@
+export  const htmlEncodeByRegExp=(str: string)=>{
+  var s = '';
+  if(str.length === 0) {
+    return '';
+  }
+  s = str.replace(/&/g,'&amp;');
+  s = s.replace(/</g,'&lt;');
+  s = s.replace(/>/g,'&gt;');
+  s = s.replace(/ /g,'&nbsp;');
+  s = s.replace(/\'/g,'&#39;');
+  s= s.replace(/\"/g,'&quot;');
+  return s;
+}
